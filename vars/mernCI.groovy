bd1fe2f275e82d821a7f464ea00562dbd8958999
Adi <kariveda11@gmail.com>
2024-11-23 00:42:54 +0530

    Introducing JSL in Mern CI pipelines

--- vars/mernCI.groovy
+++ vars/mernCI.groovy
@@ -1,4 +1,8 @@
 def call() {
+    sh 'echo ...'
+    sh 'executing this stage from jsl'
+    sh 'echo ...'
+    
     sh 'docker system prune -f'
     sh 'docker container prune -f'
     sh 'docker build -t ${AWS_ECR_REPO_NAME} .'
