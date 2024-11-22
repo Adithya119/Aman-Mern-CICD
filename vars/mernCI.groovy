ed08c9afc8c7777ad06425e4d27347d484c1e84d
Adi <kariveda11@gmail.com>
2024-11-23 01:00:59 +0530

    fix errors

--- vars/mernCI.groovy
+++ vars/mernCI.groovy
@@ -1,5 +1,5 @@
 def call() {
-    sh 'executing this stage from jsl'   
+    sh 'echo executing this stage from jsl'   
     sh 'docker system prune -f'
     sh 'docker container prune -f'
     sh 'docker build -t ${AWS_ECR_REPO_NAME} .'
