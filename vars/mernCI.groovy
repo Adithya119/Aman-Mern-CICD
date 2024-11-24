39eabc25fcd974285475f149441df09db591657e
Adi <kariveda11@gmail.com>
2024-11-25 01:01:14 +0530

    fixed errors

--- vars/mernCI.groovy
+++ vars/mernCI.groovy
@@ -5,57 +5,29 @@ def call(String blockName) {                     // By defining def call(), you 
       case 'CleaningWorkspaceAndCheckoutFromGit':       // If the block name is 'CleaningWorkspaceAndCheckoutFromGit'
         
         return {                                             // Return a closure (a reusable block of code)
-          
-          stage('Cleaning Workspace') {
-              steps {
-                  cleanWs()
-              }
-          }
-          
-          stage('Checkout from Git') {
-              steps {
-                  git url: 'https://github.com/Adithya119/Aman-Mern-CICD.git'
-              }
-          }
+            echo "Cleaning Workspace, from JSL"
+            cleanWs()
+            echo "Checkout Git, from JSL"
+            git url: 'https://github.com/Adithya119/Aman-Mern-CICD.git'
+        } 
 
-        }
-
-      
+              
       case 'QualityCheck':
         
         return {
-
-          stage('Quality Check') {
-              steps {
-                  script {
-                      waitForQualityGate abortPipeline: false, credentialsId: 'SONAR_TOKEN'   // you can choose abortPipeline to be "true"
-                  }
-              }
-          }
-
+            waitForQualityGate abortPipeline: false, credentialsId: 'SONAR_TOKEN'   // you can choose abortPipeline to be "true"
         }
 
 
       case 'EcrImagePushAndCheckoutCode':
 
         return {
-        
-          stage("ECR Image Pushing") {
-              steps {
-                  script {
-                          sh 'aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${REPOSITORY_URI}'
-                          sh 'docker tag ${AWS_ECR_REPO_NAME} ${REPOSITORY_URI}${AWS_ECR_REPO_NAME}:${BUILD_NUMBER}'
-                          sh 'docker push ${REPOSITORY_URI}${AWS_ECR_REPO_NAME}:${BUILD_NUMBER}'
-                  }
-              }
-          }
-          
-          stage('Checkout Code') {
-              steps {
-                  git url: 'https://github.com/Adithya119/Aman-Mern-CICD.git'
-              }
-          }
-
+            echo "ECR image push, from JSL"
+            sh 'aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${REPOSITORY_URI}'
+            sh 'docker tag ${AWS_ECR_REPO_NAME} ${REPOSITORY_URI}${AWS_ECR_REPO_NAME}:${BUILD_NUMBER}'
+            sh 'docker push ${REPOSITORY_URI}${AWS_ECR_REPO_NAME}:${BUILD_NUMBER}'
+            echo "Checkout Code from JSL"
+            git url: 'https://github.com/Adithya119/Aman-Mern-CICD.git'
         }
     }
 }
